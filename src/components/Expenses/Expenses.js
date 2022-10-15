abd2125ee7a9095a1908bc3d6101bd5df118a4d5
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2022-10-15 17:34:38 +0530

    Add expenses list component to refactor expenses component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,40 +1,26 @@
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
   const [selectedYear, setSelectedYear] = useState("2019");
   const setSelectedYearHandler = (year) => {
-    console.log("In expenses.js");
-    console.log(selectedYear);
     setSelectedYear(year);
-    console.log(year);
   };
 
   const filteredExpenses = props.expenseItems.filter((expense) => {
     return expense.date.getFullYear().toString() === selectedYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      ></ExpenseItem>
-    ));
-  }
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={selectedYear}
         onSelectingYear={setSelectedYearHandler}
       ></ExpensesFilter>
-      {expensesContent}
+      <ExpensesList items={filteredExpenses}></ExpensesList>
     </Card>
   );
 }
