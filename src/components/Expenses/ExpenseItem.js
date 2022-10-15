240c6c1bcf2ca2fc8fbc97947bdbfac85ef33b49
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2022-10-15 18:28:07 +0530

    Format expense item

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,14 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import { useState } from "react";
+
 function ExpenseItem(props) {
   return (
     <li>
       <Card className="expense-item">
         <ExpenseDate date={props.date}></ExpenseDate>
         <div className="expense-item__description">
-          <h2>{title}</h2>
+          <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
       </Card>
